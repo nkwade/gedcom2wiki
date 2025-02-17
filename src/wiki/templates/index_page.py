b44c4fe6e96cd84affb3444614990c57f31808b6
nkwade <nicholas@wade.dev>
2025-02-17 16:00:23 -0500

    Added the ability to collapse each section in the index page

--- src/wiki/templates/index_page.py
+++ src/wiki/templates/index_page.py
@@ -28,12 +28,20 @@ def render_index_page(family_tree: FamilyTree) -> str:
                 header_info += f"<li>{sfact.tag.value}: {sfact.value}</li>"
             header_info += "</ul>"
 
-    family_list = "<h2>Families</h2><ul>"
+    family_list = (
+        "<h2 onclick=\"toggleSection('families')\">Families &#9660;</h2>"
+        '<div id="families" style="display:block;">'
+        "<ul>"
+    )
     for fam_id, family in family_tree.families.items():
         family_list += f'<li><a href="families/{fam_id}.html">{family.name}</a></li>'
-    family_list += "</ul>"
+    family_list += "</ul></div>"
 
-    person_list = "<h2>People</h2><ul>"
+    person_list = (
+        "<h2 onclick=\"toggleSection('people')\">People &#9660;</h2>"
+        '<div id="people" style="display:block;">'
+        "<ul>"
+    )
     # Sort people by last word in their name if available
     sorted_persons = sorted(
         family_tree.persons.items(),
@@ -46,9 +54,13 @@ def render_index_page(family_tree: FamilyTree) -> str:
     for person_id, person in sorted_persons:
         name_display = person.name if person.name else person_id
         person_list += f'<li><a href="persons/{person_id}.html">{name_display}</a></li>'
-    person_list += "</ul>"
+    person_list += "</ul></div>"
 
-    source_list = "<h2>Sources</h2><ul>"
+    source_list = (
+        "<h2 onclick=\"toggleSection('sources')\">Sources &#9660;</h2>"
+        '<div id="sources" style="display:block;">'
+        "<ul>"
+    )
     sorted_sources = sorted(
         family_tree.sources.items(),
         key=lambda item: item[1].title if item[1].title else item[0],
@@ -58,7 +70,7 @@ def render_index_page(family_tree: FamilyTree) -> str:
         source_list += (
             f'<li><a href="sources/{source_id}.html">{title_display}</a></li>'
         )
-    source_list += "</ul>"
+    source_list += "</ul></div>"
 
     validation_report_link = "<h2>Data Validation Report</h2><p><a href='validation.html'>View Validation Report</a></p>"
 
@@ -69,5 +81,12 @@ def render_index_page(family_tree: FamilyTree) -> str:
         f"{person_list}"
         f"{source_list}"
         f"{validation_report_link}"
+        f"<script>"
+        "function toggleSection(id) {"
+        "  var el = document.getElementById(id);"
+        '  if(el.style.display==="none"){el.style.display="block";}'
+        '  else{el.style.display="none";}'
+        "}"
+        "</script>"
     )
     return html_page("Family Tree Wiki", content)
